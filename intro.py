2c62add71f78f8d4a678320cebcd9a37de80bf14
Ridwan Oladipupo <45660716+techcodify@users.noreply.github.com>
2018-12-06 20:51:49 +0100

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,25 @@
+#Introduction to python programming
+#To start with, a Python program that output 'Hello World'
+#Demostrating the basic Input and output statement in Python
+
+
+print("Hello World")
+
+#In other to demonstrate the basic input and output statement in Python,
+#we can demostrate by writing a program that allows users to input his/her Biodata, and output the entries respectively
+
+print("Welcome to the Biodata entry page")
+
+#Inputing..
+firstname = input("Enter your Firstname:")
+lastname = input("Enter your Lastname:")
+age = int(input("How old are you?:"))
+schoolname = input("Which school did you attend:")
+nationality = input("What is your nationality:")
+
+#Outputing..
+print("Your firstname is:", firstname)
+print("Your lastname is:", lastname)
+print("Your age is:", age)
+print("Your school is:", schoolname)
+print("Your nationality is:", nationality)
