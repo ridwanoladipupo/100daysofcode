f82e91c962ecc2e61f9f734c91f108348318b18b
harrid <oladipuporidwanolayinka@gmail.com>
2018-12-23 18:47:06 +0100

    latest commit

--- loop.py
+++ loop.py
@@ -1,3 +1,16 @@
+"""
+Task 
+Read an integer . For all non-negative integers , print . See the sample for details.
+
+Input Format
+The first and only line contains the integer, .
+Constraints
+1 <= n <= 100
+
+Output Format
+Print n lines, one corresponding to each .
+"""
+
 if __name__ == '__main__':
     n = int(input(""))
 
